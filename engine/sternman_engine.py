4306e4d4a306f300a4b3c801d3b801b47938d08b
Mahmoud <m.e.bestawy@gmail.com>
2022-08-17 21:01:30 -0400

    concrete engine classes

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,12 @@
-from abc import ABC
 
-from car import Car
+from engine.engine import Engine
 
+class SternmanEngine(Engine):
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
+    __warning_light_is_on: bool
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+    def __init__(self, warning_light_is_on: bool):
+        self.__warning_light_is_on = warning_light_is_on
+
+    def needs_service(self) -> bool:
+        return self.__warning_light_is_on
