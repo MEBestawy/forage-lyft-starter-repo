5ea9c5473027f7f5d9dc21671d3229e19a9fb231
Mahmoud <m.e.bestawy@gmail.com>
2022-08-17 20:48:43 -0400

    SpindlerBattery

---
+++ battery/spindler_battery.py
@@ -0,0 +1,17 @@
+
+from datetime import date
+
+from battery.battery import Battery
+
+
+class SpindlerBattery(Battery):
+
+    __last_service_date: date
+    __current_date: date
+    
+    def __init__(self, last_service_date, current_date):
+        self.__last_service_date = last_service_date
+        self.__current_date = current_date
+
+    def needs_service(self) -> bool:
+        return (self.__current_date.year - self.__last_service_date.year) >= 2
